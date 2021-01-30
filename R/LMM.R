31ea6da0540d6ce07f8beea4867d2a0a16ac7e66
Anders Hagen Jarmund <anders.h.jarmund@ntnu.no>
2021-01-30 10:49:15 +0100

    P value adjustment

--- R/LMM.R
+++ R/LMM.R
@@ -39,10 +39,12 @@ getLMECoefficients <- function(object){
   colnames(fdf) <- c("covar", "estimate", "pvalue", "variable")
   
   
-  #if(!is.na(object$pAdjustMethod)){
-    #object$LMM.coefficients$pvalue_adj <- p.adjust(object$LMM.coefficients$pvalue, method = object$pAdjustMethod)
-  cat("P value correction deactivated for now...\n")
-  #}
+  if(!is.na(object$pAdjustMethod)){
+    object$LMM.coefficients$pvalue_adj <- NA
+    for(i in unique(object$LMM.coefficients$covar)){
+      object$LMM.coefficients$pvalue_adj[object$LMM.coefficients$covar == i, ] <- p.adjust(object$LMM.coefficients$pvalue[object$LMM.coefficients$covar == i, ], method = object$pAdjustMethod)
+    }
+  }
   object$LMM.coefficients <- fdf
   end.time <- Sys.time()
   cat("- Time: ", end.time - start.time, "\n")
@@ -80,73 +82,12 @@ separateLMECoefficients <- function(object){
   return(object)
 }
 
-getEffectMatrix2 <- function(object){
-  if(!object$minimizeObject){
-    cat("Calculating effect matrix (may take some time...)\n")
-  }
-  parts <- subset(object$df, variable == object$df$variable[1]) # just to get time and group for each participant
-
-  covars <- unique(object$LMM.coefficients$covar)
-  refTime <- levels(object$df$time)[1]
-  refGroup <- levels(object$df$group)[1]
-  fdf_pred <- data.frame()
-
-  # if time and group effects are separated, we need to do that here:
-  if(object$separateTimeAndGroup){
-    fdf_time_pred <- data.frame()
-    fdf_group_pred <- data.frame()
-
-    for(j in 1:length(covars)){
-      for(i in 1:nrow(parts)){
-        fdf_time_pred[i,j] <- object$LMM.coefficients$estimate[object$LMM.coefficients$covar == covars[j] & object$LMM.coefficients$variable == "(Intercept)"]
-        if(parts$time[i] != refTime){
-          fdf_time_pred[i,j] <- fdf_time_pred[i,j] +object$LMM.coefficients$estimate[object$LMM.coefficients$covar == covars[j] & object$LMM.coefficients$variable == paste0("time",parts$time[i])]
-        }
-        fdf_group_pred[i,j] <- 0
-        if(parts$group[i] != refGroup){
-          if(object$hasGroupTerm){
-            fdf_group_pred[i,j] <- object$LMM.coefficients$estimate[object$LMM.coefficients$covar == covars[j] & object$LMM.coefficients$variable == paste0("group",parts$group[i])]
-          }
-          if(parts$time[i] != refTime & object$hasInteractionTerm){
-            fdf_group_pred[i,j] <- fdf_group_pred[i,j] + object$LMM.coefficients$estimate[object$LMM.coefficients$covar == covars[j] & object$LMM.coefficients$variable == paste0("time",parts$time[i],":group",parts$group[i])]
-          }
-        }
-      }
-    }
-    fdf_time_pred$comp <- "TIME"
-    fdf_group_pred$comp <- "GROUP"
-    fdf_pred <- rbind(fdf_time_pred, fdf_group_pred)
-  }else{
-    for(i in 1:nrow(parts)){
-      for(j in 1:length(covars)){
-
-        fdf_pred[i,j] <- object$LMM.coefficients$estimate[object$LMM.coefficients$covar == covars[j] & object$LMM.coefficients$variable == "(Intercept)"]
-        if(parts$time[i] != refTime){
-          fdf_pred[i,j] <- fdf_pred[i,j] + object$LMM.coefficients$estimate[object$LMM.coefficients$covar == covars[j] & object$LMM.coefficients$variable == paste0("time",parts$time[i])]
-        }
-        if(parts$group[i] != refGroup){
-          if(object$hasGroupTerm){
-            fdf_pred[i,j] <- fdf_pred[i,j] + object$LMM.coefficients$estimate[object$LMM.coefficients$covar == covars[j] & object$LMM.coefficients$variable == paste0("group",parts$group[i])]
-          }
-          if(parts$time[i] != refTime & object$hasInteractionTerm){
-            fdf_pred[i,j] <- fdf_pred[i,j] + object$LMM.coefficients$estimate[object$LMM.coefficients$covar == covars[j] & object$LMM.coefficients$variable == paste0("time",parts$time[i],":group",parts$group[i])]
-          }
-        }
-
-      }
-    }
-    fdf_pred$comp <- "TIME"
-  }
-  colnames(fdf_pred) <- c(as.character(covars),"comp")
-  object$effect.matrix <- fdf_pred
-  object$parts$time <- parts$time
-  object$parts$group <- parts$group
-  if(!object$minimizeObject){
-    cat("Finished calculating effect matrix!\n")
-  }
-  return(object)
-}
-
+#' Get effect matrix
+#'
+#' This function separates time and group variables if separateTimeAndGroup = TRUE
+#'
+#' @param object An RMASCA object
+#' @return An RMASCA object
 getEffectMatrix <- function(object){
   if(!object$minimizeObject){
     cat("Calculating effect matrix (may take some time...)\n")
