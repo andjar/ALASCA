b8cc17b82ee6bf6969282af4fd474c9743af0084
Anders Hagen Jarmund <anders.h.jarmund@ntnu.no>
2022-05-22 11:32:22 +0200

    v0.0.0.99

--- R/AlascaModel.R
+++ R/AlascaModel.R
@@ -19,9 +19,9 @@ AlascaModel <- R6::R6Class("AlascaModel",
     #' @field ignore_missing_covars If TRUE, ignore missing covariate values
     ignore_missing_covars = FALSE,
     #' @field version Version number
-    version = "0.0.0.98",
+    version = "0.0.0.99",
     #' @field update_date Date of latest update
-    update_date = "2022-05-10",
+    update_date = "2022-05-22",
 
     # Effect matrices
     #' @field separate_effects If TRUE, try to separate the effects
